1e37b6c86e51255cf0ef7f8ad0a5f3649f50e337
Martien Dermawan Tanama <martiendt@gmail.com>
2023-12-21 12:35:10 +0700

    dsa

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,6 +5,8 @@ import { DatabaseTestUtil } from './utils'
 beforeAll(async () => {
   console.log('initiate database connection')
   await DatabaseTestUtil.open()
+  console.log('generate database collection schema')
+  await DatabaseTestUtil.createCollections()
 })
 
 afterAll(async () => {
