f03b06528dbcb578bbadf67b91891bf42ba6a252
Martien Dermawan Tanama <martiendt@gmail.com>
2023-12-26 00:16:55 +0700

    dsa

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { createServer } from './server'
 /**
  * Create database connection. It will keep the connection open by default,
  * and use the same connection for all queries. If you need to close the connection,
- * call dbConnection.close() (which is asynchronous and returns a Promise).
+ * call dbConnection.close() (which is asynchronous and returns a Promise)..
  */
 await dbConnection.open()
 
